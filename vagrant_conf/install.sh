1a9193f9decd92f8486d7245634292486bf5b2ff
Behnisch <behnisch13@gmail.com>
2017-12-11 00:10:28 +0500

    added lessons

--- vagrant_conf/install.sh
+++ vagrant_conf/install.sh
@@ -3,7 +3,14 @@
 sudo apt-get update -y
 sudo apt-get install vim python-virtualenv python3-dev python3-setuptools -y
 
-sudo easy_install3 "pip==7.1.2"
+#sudo easy_install3 "pip==7.1.2"
+cd /vagrant/vagrant_conf
+tar xvfz pip-7.1.2.tar.gz
+cd pip-7.1.2
+sudo python3.2 setup.py install
+cd ..
+sudo rm -fr pip-7.1.2
+cd /
 
 cp /vagrant/example.db /vagrant/pythontutor.db
 
